4383e0c3264b4c8281fb7f60689995e4b70356b1
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-01-28 05:41:35 -0800

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - 0-7
+ * Return: 0
+ */
+int main(void)
+{
+	int ct = 0;
+
+	while (ct < 10)
+	{
+		printf("%d", ct);
+		ct++;
+	}
+	printf("\n");
+	return (0);
+}
