12d7b601028c7bac8ba46650c6d4ce79e93c5c66
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-01-31 07:44:41 -0800

    chked c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+ * _isalpha - checks aphla
+ * @c: to cheked
+ * Retrun: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return(0);	
+}
