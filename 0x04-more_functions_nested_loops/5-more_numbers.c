68ec4e3379fa1571da348d4965770577f4f5d6fb
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-02-15 06:28:23 -0800

    14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * more_numbers - cft
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i < 14)
+	{
+		int j = 1;
+		int c = 0;
+
+		while (c < 2)
+		{
+		int k = 0;
+
+		while (k < 9)
+		{
+			if ((c == 1 && k < 5) || c == 0)
+			{
+			if (c == 1)
+			{
+				_putchar(j + '0');
+			}
+			_putchar(k + '0');
+			}
+			k++;
+		}
+		c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
