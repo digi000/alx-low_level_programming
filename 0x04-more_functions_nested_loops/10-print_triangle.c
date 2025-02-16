8d22686fd6987ae6183938f9451f53aca4cccd65
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-02-16 08:38:03 -0800

    adf

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - gh
+ * @size: ssf
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+	int fp = 0;
+	int j = size;
+	int m = 0;
+	int l = 0;
+
+	while (i < size)
+	{
+		j--;
+		fp++;
+		m = 0;
+
+		while (m < j && j != 0)
+		{
+			_putchar(' ');
+			m++;
+		}
+		l = 0;
+
+		while (l < fp)
+		{
+			_putchar('#');
+			l++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
