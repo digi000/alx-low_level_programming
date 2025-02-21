8019643476b6cdb42b9fe18d3f8beda0bf7e5919
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-02-20 18:45:59 -0800

    pr

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - pt ch
+ * @s: chk
+ */
+void print_rev(char *s)
+{
+	int ct = 0;
+	while (s[ct] != '\0')
+	{
+		ct++;
+	}
+	while (ct >= 0)
+	{
+		_putchar(s[ct]);
+		ct--;
+	}
+	_putchar('\n');
+}
