9eac2181c2248ae96226d9162fab7de7bea066b5
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-14 14:18:26 +0200

    23

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - frf
+ * @argc: jh
+ * @argv: iuh
+ * Return: iuhi
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
