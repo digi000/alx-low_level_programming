8c7f8074293bf3e37f280977b367b25777d37245
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-17 10:29:18 +0200

    4s

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *myd;
+
+	myd = malloc(sizeof(dog_t));
+	if (myd == NULL)
+		return (NULL);
+	myd->name = malloc(strlen(name) + 1);
+	myd->owner = malloc(strlen(owner) + 1);
+	strcpy(myd->name, name);
+	strcpy(myd->owner, owner);
+	myd->age = age;
+	return (myd);
+}
