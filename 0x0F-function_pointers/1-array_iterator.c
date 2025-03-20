714fece333b2f3b4ebadc52f1edfefbe8f976a7f
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-20 09:37:12 +0200

    fp1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include<stddef.h>
+/**
+ * array_iterator - kj
+ * @array: jh
+ * @size: sds
+ * @action: dre
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i < (int)size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
