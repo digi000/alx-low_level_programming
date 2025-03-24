390b0a46c885f3b76e820c4588b3140d2dfa0055
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-24 14:45:23 +0200

    ss61

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,19 +3,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t* current;
-	listint_t* temp;
 	int rt;
 
 	current = *head;
 	if ((*head)->n)
 		rt = (*head)->n;
 	else
-		rt = 0;
-	while (current)
-	{
-		temp = current;
-		current = current->next;
-		free(temp);
-	}
+		return (0);
+	free(current);
 	return (rt);
 }
