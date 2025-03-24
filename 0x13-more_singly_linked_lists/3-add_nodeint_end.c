04b1c252b57c3ad5328194c3ed146674191d1d22
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-24 13:31:14 +0200

    ss3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t* newnode;
+	listint_t* current;
+
+	current = *head;	
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (*head);
+	}
+	while(current->next)
+	{
+		current = current->next;
+	}
+	current->next = newnode;
+	return (newnode);
+}
