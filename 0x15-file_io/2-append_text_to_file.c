f07ca8a5360cbd4caa450e273d44b97aa0a7a85c
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-27 18:13:32 +0200

    f2-

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,4 @@
-#include <stdib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -7,16 +7,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, wr, i;
 	if (filename == NULL)
 		return (-1);
-	if !(access(filename, W_OK) == 0)
-		return (-1);
-	for (i = 0; filename[i] != '\0'; i++)
+	
+	for (i = 0; text_content[i] != '\0'; i++)
 	{}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	wr = write(fd, filename, sizeof(char) * i);
+	wr = write(fd, text_content, sizeof(char) * i);
 	if (wr == -1)
 		return (-1);
-	return (1);
 	close(fd);
+	return (1);
 }
