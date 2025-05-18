2edc297e91e88946a30d08dc0f33b4e23f0f6fcb
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-18 13:55:21 +0200

    5t

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+def island_perimeter(grid):
+    rw = len(grid)
+    cl = len(grid[0])
+    i = 0
+    length = 0
+    column = 0
+    listy = 0
+    while i < rw:
+        j = 0
+        while j < cl:
+            if grid[i][j] == 0:
+                pass
+            elif grid[i][j] == 1 and length < 1:
+                length += 1
+                column = j
+                listy = i
+            else:
+                if column == j:
+                    length += 1
+                    column = j
+                    listy = i
+                elif listy == i and j - column == 1:
+                    length += 1
+                    column = j
+                    listy = i
+            j += 1
+        i += 1
+    return (length * 2) + 2
